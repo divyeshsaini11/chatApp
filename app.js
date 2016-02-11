44454be53c91fed1587454898c42a37942b2f9a9
divyeshsaini11 <techdivyesh11@gmail.com>
2016-02-11 23:15:47 +0530

    Added private chating using username(unique) as id

--- app.js
+++ app.js
@@ -2,40 +2,57 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
-	nicknames = [];
-
+	users = {};
+	
 server.listen(3000);
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){
+	res.sendfile(__dirname + '/index.html');
 });
 
-//socket functionality on server side
-io.sockets.on('connection', function(socket) { //turn ON the connection event when client tries to connect
-	socket.on('new user', function(data, callback) {
-		if(nicknames.indexOf(data) != -1) {
+io.sockets.on('connection', function(socket){
+	socket.on('new user', function(data, callback){
+		if (data in users){
 			callback(false);
-		} else {
+		} else{
 			callback(true);
 			socket.nickname = data;
-			nicknames.push(socket.nickname);
+			users[socket.nickname] = socket;
 			updateNicknames();
 		}
 	});
-
-	function updateNicknames() {
-		io.sockets.emit('usernames', nicknames);
+	
+	function updateNicknames(){
+		io.sockets.emit('usernames', Object.keys(users));
 	}
-	socket.on('send message', function(data) {
-		//io.sockets.emit('new message', data); //username doesnot appear on chatting
-		  io.sockets.emit('new message', {msg: data, nick: socket.nickname});
-		//socket.broadcast.emit('new message', data);
-	});
- 
-	socket.on('disconnect', function(data) {
-		if(!socket.nickname) return;
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
-		updateNicknames();
-	})
 
+	socket.on('send message', function(data, callback){
+		var msg = data.trim();
+		console.log('after trimming message is: ' + msg);
+		if(msg.substr(0,3) === '/w '){
+			msg = msg.substr(3);
+			var ind = msg.indexOf(' ');
+			if(ind !== -1){
+				var name = msg.substring(0, ind);
+				var msg = msg.substring(ind + 1);
+				if(name in users){
+					users[name].emit('whisper', {msg: msg, nick: socket.nickname});
+					console.log('message sent is: ' + msg);
+					console.log('Whisper!');
+				} else{
+					callback('Error!  Enter a valid user.');
+				}
+			} else{
+				callback('Error!  Please enter a message for your whisper.');
+			}
+		} else{
+			io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
+		}
+	});
+	
+	socket.on('disconnect', function(data){
+		if(!socket.nickname) return;
+		delete users[socket.nickname];
+		updateNicknames();
+	});
 });
